d733506d83c8d9589c98bf3c5f13aa8d4ffa429b
citizengit <kriss.heimanis@au.pwc.com>
2014-10-20 14:48:46 +1100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'css/soundgrid.css' : 'sass/soundgrid.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
+
+  grunt.registerTask('serve', function (target) {});
+
+  grunt.registerTask('server', 'Start a custom web server.', function() {
+    grunt.log.writeln('Starting web server on port 1234.');
+    require('./server.js').listen(1234);
+  });
+};

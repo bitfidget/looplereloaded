04d900594199e0eaa456abe4a1b69faa887dfd21
citizengit <kriss.heimanis@au.pwc.com>
2014-10-22 15:47:39 +1100

    stolen metronome

---
+++ metronome-master/js/metronomeworker.js
@@ -0,0 +1,31 @@
+// massif thanks to Chris Wilson/HTML5ROCKS/https://github.com/cwilso/metronome/
+
+var timerID = null;
+var interval = 100;
+
+self.onmessage = function(e) {
+	if (e.data === "start") {
+		console.log("starting");
+		timerID = setInterval(function() {
+			postMessage("tick");
+		},interval)
+	}
+	else if (e.data.interval) {
+		console.log("setting interval");
+		interval = e.data.interval;
+		console.log("interval = "+interval);
+		if (timerID) {
+			clearInterval(timerID);
+			timerID = setInterval(function() {
+				postMessage("tick");
+			},interval)
+		}
+	}
+	else if (e.data === "stop") {
+		console.log("stopping");
+		clearInterval(timerID);
+		timerID = null;
+	}
+};
+
+postMessage('hi there');
